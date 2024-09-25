b9703035cac069265cea5f31dde753222a21b247
Lalitkumar_kamble <116297594+lalitkumar0213@users.noreply.github.com>
2024-09-25 01:31:54 -0700

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,15 @@
+function dol(){
+    let a;
+    a = id1.value + 56;
+    op.value = +a;
+}
+function eur(){
+    let b;
+    b = id1.value + 52;
+    op.value = +b;
+}
+function ye(){
+    var c;
+    c= id1.value + 560;
+    op.value = +c;
+}
